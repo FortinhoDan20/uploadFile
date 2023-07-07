0edec8e845b55cef681cc7be9d8964df13d48fb6
inspecthers <kg.geku@gmail.com>
2023-07-07 09:46:26 +0100

    file upload in progress 02

--- src/components/useFileUpload.jsx
+++ src/components/useFileUpload.jsx
@@ -1,24 +1,24 @@
-import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { toast } from "react-toastify";
 import { onUploadFile } from "../features/upload/uploadSlice";
 
 const useFileUpload = () => {
-  const [file, setfile] = useState("");
   const dispatch = useDispatch();
 
   const handleFileChange = (e) => {
     if (e.target.files) {
-      // setFile(e.target.files[0]);
-      setfile(e.target.files[0]);
-      // console.log(e.target.files[0]);
-      dispatch(onUploadFile({ image: file, toast }));
+      const fileToUpload = e.target.files[0];
+      console.log(fileToUpload);
+
+      const formData = new FormData();
+      formData.append("image", fileToUpload);
+
+      dispatch(onUploadFile({ image: fileToUpload, toast }));
     }
   };
 
   return {
     handleFileChange,
-    file,
   };
 };
 
